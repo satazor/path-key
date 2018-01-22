e135a95a33d8365ba835b46b47c55f34e0218501
André Cruz <andremiguelcruz@msn.com>
2018-01-22 11:42:57 +0000

    fix windows shell, which runs ubuntu, not detecting Path correctly

--- index.js
+++ index.js
@@ -5,9 +5,24 @@ module.exports = opts => {
 	const env = opts.env || process.env;
 	const platform = opts.platform || process.platform;
 
-	if (platform !== 'win32') {
+	// Short-circuit if PATH exists, which is the most common
+	if (Object.prototype.hasOwnProperty.call(env, 'PATH')) {
 		return 'PATH';
 	}
 
-	return Object.keys(env).find(x => x.toUpperCase() === 'PATH') || 'Path';
+	// Short-circuit if PATH exists, which is the second most common
+	// e.g.: Windows or Windows bash which runs Ubuntu
+	if (Object.prototype.hasOwnProperty.call(env, 'Path')) {
+		return 'Path';
+	}
+
+	// Look for PATH in a case insensitive manner
+	const found = Object.keys(env).find(x => x.toUpperCase() === 'PATH');
+
+	if (found) {
+		return found;
+	}
+
+	// If none were found, have a good fallback based on the OS
+	return platform === 'win32' ? 'Path' : 'PATH';
 };
--- test.js
+++ test.js
@@ -4,7 +4,10 @@ import m from './';
 test(t => {
 	t.is(m().toUpperCase(), 'PATH');
 	t.is(m({env: {PATH: ''}}), 'PATH');
-	t.is(m({env: {Path: ''}, platform: 'win32'}), 'Path');
+	t.is(m({env: {Path: ''}}), 'Path');
+	t.is(m({env: {PATh: ''}}), 'PATh');
+	t.is(m({env: {Path: '', PATH: ''}}), 'PATH');
+	t.is(m({env: {}, platform: 'linux'}), 'PATH');
 	t.is(m({env: {}, platform: 'darwin'}), 'PATH');
 	t.is(m({env: {}, platform: 'win32'}), 'Path');
 });
